7c97d0a1529026c498867e26250d23ae3b43c38e
Josh Rueschenberg <joshru@uw.edu>
2018-04-18 18:42:00 -0700

    Test groupme bot portion of application.

--- main.go
+++ main.go
@@ -6,12 +6,13 @@ import (
 	"net/http"
 	"log"
 	"os"
+	"github.com/sha1sum/golang_groupme_bot/bot"
 )
 //Random track ID: 1IruBrVHO0XS9SfXGoYBXn
 //playlist ID: 4jj4dm7CryepjBlKwT4dKe
 
 //const redirectURL = "https://open.spotify.com/user/rooshypooshy/playlist/4jj4dm7CryepjBlKwT4dKe?si=tiUyT3x-QWSJEGBvUEQ7xw"
-//const redirectURL = "http://localhost:8080/"
+//const redirectURL = "http://localhost:8080"
 const redirectURL = "https://groupme-botify.herokuapp.com"
 
 var (
@@ -21,6 +22,17 @@ var (
 	ch          = make(chan *spotify.Client)
 	auth = spotify.NewAuthenticator(redirectURL, spotify.ScopeUserReadPrivate, spotify.ScopeUserLibraryRead, spotify.ScopePlaylistModifyPublic)
 )
+
+type Handler struct{}
+
+func (handler Handler) Handle(term string, c chan []*bot.OutgoingMessage, message bot.IncomingMessage) {
+	fmt.Println("Handler called!")
+	// exit early if the received message was posted by a bot
+	if message.SenderType == "bot" {
+		return
+	}
+	fmt.Println("Found message:", message.Text)
+}
 
 // Begin Spotify authorization flow, after user logs in they will be redirected to a success page
 // https://godoc.org/github.com/zmb3/spotify#Authenticator
@@ -44,7 +56,7 @@ func main() {
 	fmt.Println("Starting Botify!")
 
 	// fetch port from Heroku, or use 8080 if no port environment variable is set
-	port := os.Getenv("PORT")
+/*	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
@@ -70,8 +82,21 @@ func main() {
 		log.Fatal(err)
 	}
 	client.AddTracksToPlaylist("rooshypooshy", "4jj4dm7CryepjBlKwT4dKe", "1IruBrVHO0XS9SfXGoYBXn")
+	fmt.Println("You are logged in as:", user.ID)*/
 
-	fmt.Println("You are logged in as:", user.ID)
+	fmt.Println("Creating groupme bot")
+	commands := make([]bot.Command, 0)
+	songs := bot.Command{
+		Triggers: []string {
+			"https://open.spotify",
+			"testing",
+			"",
+		},
+		Handler: new(Handler),
+		BotID: "d01b6e91b7c35b66405ba58dbf",
+	}
+	commands = append(commands, songs)
+	bot.Listen(commands)
 
 	// block forever
 	select {}
